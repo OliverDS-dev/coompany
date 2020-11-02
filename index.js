32454ea2247554be66abd4f362211cc712eca260
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-02 11:22:08 +0100

    reee

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let http = require('http');
 // process.env.PORT lets the port be set by heruko
 let port = process.env.PORT || 8080;
 
-http.createServer(function(request, response){
-    response.writeHead(200);
-    response.write("Hello World");
-    response.end();
+http.createServer(function(req, res){
+    res.writeHead(200);
+    res.write("Hello World");
+    res.end();
 }).listen(port);
 
 console.log("Listening on the port " + port + "...");
