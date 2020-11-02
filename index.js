8920f9ea4cb91fc5e1b5357178296a1517676046
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-02 11:14:20 +0100

    sdf

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let http = require('http');
 // process.env.PORT lets the port be set by heruko
 let port = process.env.PORT || 8080;
 
-http.createServer(function(request, respond){
-    Response.writeHead(200);
-    Response.write("Hello World");
-    Response.end();
+http.createServer(function(request, response){
+    response.writeHead(200);
+    response.write("Hello World");
+    response.end();
 }).listen(port);
 
 console.log("Listening on the port " + port + "...");
